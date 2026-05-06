cef26d309be5e69aa27035933255009c04036d5a
DanielArg100 <danicaffe100@gmail.com>
2026-05-06 16:36:01 -0300

    FunciónJS

--- scripts/db.js
+++ scripts/db.js
@@ -1,76 +1,76 @@
 export const ListaProductos = [{
+    nombre: "Daniel Cafferata Buhonero",
+    descripción: "¡Atención, forasteros! 🧟‍♂️ Añade a tu colección al personaje más misterioso, carismático y útil de todo el universo de Resident Evil con esta increíble figura Funko Pop! inspirada en El Buhonero (The Merchant) de Resident Evil 4 (Remake 2023).",
+    precio: 16000,
+    img: "src/SRC/Perfil.png"
+}, {
+    nombre: "Bruno Araujo Leon S. Kennedy",
+    descripción: "¿Eres fan de la élite de la BSAA? ¡No dejes escapar el Funko Pop de Umber Eyes (Rolando Elba)! Como francotirador y segundo al mando de Chris Redfield en Resident Evil Village, esta figura captura su look táctico listo para la operación en Europa del Este. Con su equipo de combate de alta tecnología y su porte imponente, Umber Eyes es la pieza de acción que le falta a tu vitrina de Resident Evil. ¡Haz tu preventa/compra ahora y asegura al especialista de la unidad Hound Wolf antes de que se agote!",
+    precio: 30000,
+    img: "src/SRC/Perfil.png"
+}, {
+    nombre: "Joaquin Paz Nemesis",
+    descripción: "¡STARS...! Revive el pánico de Raccoon City con este espectacular Funko Pop de Nemesis, el arma biológica definitiva de Umbrella Corporation. Esta figura captura la esencia del antagonista más implacable de Resident Evil 3. Con su bazuca en mano y su rostro característico lleno de cicatrices, este pequeño titán de vinilo de 3.75 pulgadas está diseñado para dominar tu estantería. Si eres fan del terror de supervivencia, este Pop es una joya 'vaulted' (descatalogada) que no puede faltar.",
+    precio: 25000,
+    img: "src/SRC/Perfil.png"
+}, {
+    nombre: "Federico Travella Umder Eyes",
+    descripción: "¿Eres fan de la élite de la BSAA? ¡No dejes escapar el Funko Pop de Umber Eyes (Rolando Elba)! Como francotirador y segundo al mando de Chris Redfield en Resident Evil Village, esta figura captura su look táctico listo para la operación en Europa del Este. Con su equipo de combate de alta tecnología y su porte imponente, Umber Eyes es la pieza de acción que le falta a tu vitrina de Resident Evil. ¡Haz tu preventa/compra ahora y asegura al especialista de la unidad Hound Wolf antes de que se agote!",
+    precio: 21000,
+    img: "src/SRC/Perfil.png"
+}, {
+    nombre: "Pablo Gareis",
+    descripción: "",
+    precio: 0,
+    img: "src/SRC/Perfil.png"
+}, {
     nombre: "Funko Pop Daniel Leon S. Kennedy",
     descripción: "",
     precio: 0,
-    img: "src/"
-},{
-    nombre: "Funko Pop Bruno Chris Redfield",
-    descripción: "",
-    precio: 0,
-    img: "src/"
-},{
+    img: "src/SRC/Perfil.png"
+}, {
     nombre: "Funko Pop Daniel Leon S. Kennedy",
     descripción: "",
     precio: 0,
-    img: "src/"
-},{
+    img: "src/SRC/Perfil.png"
+}, {
     nombre: "Funko Pop Daniel Leon S. Kennedy",
     descripción: "",
     precio: 0,
-    img: "src/"
-},{
+    img: "src/SRC/Perfil.png"
+}, {
     nombre: "Funko Pop Daniel Leon S. Kennedy",
     descripción: "",
     precio: 0,
-    img: "src/"
-},{
+    img: "src/SRC/Perfil.png"
+}, {
     nombre: "Funko Pop Daniel Leon S. Kennedy",
     descripción: "",
     precio: 0,
-    img: "src/"
-},{
+    img: "src/SRC/Perfil.png"
+}, {
     nombre: "Funko Pop Daniel Leon S. Kennedy",
     descripción: "",
     precio: 0,
-    img: "src/"
-},{
+    img: "src/SRC/Perfil.png"
+}, {
     nombre: "Funko Pop Daniel Leon S. Kennedy",
     descripción: "",
     precio: 0,
-    img: "src/"
-},{
+    img: "src/SRC/Perfil.png"
+}, {
     nombre: "Funko Pop Daniel Leon S. Kennedy",
     descripción: "",
     precio: 0,
-    img: "src/"
-},{
+    img: "src/SRC/Perfil.png"
+}, {
     nombre: "Funko Pop Daniel Leon S. Kennedy",
     descripción: "",
     precio: 0,
-    img: "src/"
-},{
+    img: "src/SRC/Perfil.png"
+}, {
     nombre: "Funko Pop Daniel Leon S. Kennedy",
     descripción: "",
     precio: 0,
-    img: "src/"
-},{
-    nombre: "Funko Pop Daniel Leon S. Kennedy",
-    descripción: "",
-    precio: 0,
-    img: "src/"
-},{
-    nombre: "Funko Pop Daniel Leon S. Kennedy",
-    descripción: "",
-    precio: 0,
-    img: "src/"
-},{
-    nombre: "Funko Pop Daniel Leon S. Kennedy",
-    descripción: "",
-    precio: 0,
-    img: "src/"
-},{
-    nombre: "Funko Pop Daniel Leon S. Kennedy",
-    descripción: "",
-    precio: 0,
-    img: "src/"
+    img: "src/SRC/Perfil.png"
 }]
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,41 @@
-import { ListaProductos } from "./db";
+import { ListaProductos } from "./db.js";
 
-let intermedio = document.getElementsByClassName("intermedio")
+
+
+function MostrarProductos(lista) {
+    let div = document.querySelector(".contenedor")
+    div.innerHTML = ""
+    lista.forEach(i => {
+        let nuevodiv = document.createElement("div")
+        nuevodiv.className = "Tarjetas"
+        nuevodiv.innerHTML =
+        `
+            <h1>${i.nombre}</h1>
+            <div>
+                <p>${i.descripción}</p>
+                <img src${i.img}>
+            </div>
+            <p${i.precio}></p>
+        `
+        div.appendChild(nuevodiv)
+    });
+}
+
+MostrarProductos(ListaProductos)
+
+
+let div = document.querySelector(".contenedor")
+let textoCrudo = document.getElementById("texto")
+let buscar = document.querySelector("#buscar")
+
+textoCrudo.addEventListener("keyup", function () {
+    let Filtrar = ListaProductos.filter(i=>i.nombre.toLowerCase().includes(textoCrudo.value.trim().toLowerCase()))
+    if (Filtrar.length > 0) {
+        MostrarProductos(Filtrar)
+    } else {
+        div.innerHTML = `
+            <p>No se encontraron coincidencias....</p>
+        `
+    }
+})
+
